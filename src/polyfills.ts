c16af1f891cc8eb1c11df97062bcfe607ec577a1
madhushanid <madushanid@zone24x7.com>
2020-03-29 02:01:02 +0530

    Added styling for the options list

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -35,7 +35,7 @@ import 'core-js/es/array';
 import 'core-js/es/array';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js'; // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /**
  * Web Animations `@angular/platform-browser/animations`
