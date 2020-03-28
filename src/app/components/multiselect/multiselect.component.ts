541bbcbe2527ed1420c88287144f912e623f3100
Naveen C. Cooray <naveen@calcey.com>
2020-03-29 00:27:50 +0530

    Add select deselect functionality

--- src/app/components/multiselect/multiselect.component.ts
+++ src/app/components/multiselect/multiselect.component.ts
@@ -7,34 +7,61 @@ import { EmitType } from 'src/app/models/EmitOption';
   styleUrls: ['./multiselect.component.scss']
 })
 export class MultiselectComponent implements OnInit {
-  @Input() optionsList: Option[];
+  @Input() data: Option[];
   selectedOptionsList: Option[] = [];
-
-  selectAll: Option = {
-    key: 'select-all',
-    value: 'Select All'
-  };
-  selectNone: Option = {
-    key: 'select-none',
-    value: 'Select None'
-  };
+  selectAllList: Option[] = [];
+  optionsList: Option[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.optionsList = [...this.data];
+    this.selectAllList = [
+      ...this.data.map(aOption => ({
+        ...aOption,
+        isSelected: true
+      }))
+    ];
+  }
+
+  replaceAt(array: Option[], index: number, value: Option): Option[] {
+    const ret = array.slice(0);
+    ret[index] = value;
+    return ret;
+  }
 
   toggleOption = (emitOption: EmitType): void => {
     const { option, index } = emitOption;
     if (option.isSelected) {
-      this.optionsList[index].isSelected = false;
+      this.optionsList = [
+        ...this.replaceAt(this.optionsList, index, {
+          ...option,
+          isSelected: false
+        })
+      ];
       this.selectedOptionsList = [
         ...this.selectedOptionsList.filter(
           aOption => aOption.key !== option.key
         )
       ];
     } else {
-      this.optionsList[index].isSelected = true;
+      this.optionsList = [
+        ...this.replaceAt(this.optionsList, index, {
+          ...option,
+          isSelected: true
+        })
+      ];
       this.selectedOptionsList = [...this.selectedOptionsList, option];
+    }
+  };
+
+  onSelectAll = (isSelectAll: boolean): void => {
+    if (isSelectAll) {
+      this.selectedOptionsList = [...this.selectAllList];
+      this.optionsList = [...this.selectAllList];
+    } else {
+      this.selectedOptionsList = [];
+      this.optionsList = [...this.data];
     }
   };
 
